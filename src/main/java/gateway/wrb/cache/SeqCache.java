8fa743111d31112500037fd836cc55eaf28693a5
cuong <cuong8888q@gmail.com>
2019-11-01 11:33:07 +0700

    01112019 update cache Redis

--- src/main/java/gateway/wrb/cache/SeqCache.java
+++ src/main/java/gateway/wrb/cache/SeqCache.java
@@ -13,8 +13,9 @@ public class SeqCache {
     @Autowired
     SeqRepo seqRepo;
 
-    @Cacheable(value = "seqCache", key = "#id")
+    @Cacheable(value = "seqCache", key = "#id", condition = "#result != null")
     public SeqModel getItem(String id) {
+        System.out.println("SeqCache ----> getItem");
         SeqModel seqModel = null;
         try {
             seqModel = seqRepo.getItem(id);
@@ -26,16 +27,19 @@ public class SeqCache {
 
     @CacheEvict(value = "seqCache", key = "#id")
     public void deleteItem(String key) {
+        System.out.println("SeqCache -----> deleteItem");
         seqRepo.deleteItem(key);
     }
 
     @CachePut(value = "seqCache", key = "#id", condition = "#result != null")
     public void addItem(SeqModel seqModel) {
+        System.out.println("SeqCache --------> addItem");
         seqRepo.addItem(seqModel);
     }
 
     @CachePut(value = "seqCache", key = "#id", condition = "#result != null")
     public void updateItem(SeqModel seqModel) {
+        System.out.println("SeqCache --------> updateItem");
         seqRepo.updateItem(seqModel);
     }
 }
--- src/main/java/gateway/wrb/repositories/SeqRepo.java
+++ src/main/java/gateway/wrb/repositories/SeqRepo.java
@@ -1,13 +1,39 @@
 package gateway.wrb.repositories;
 
 import gateway.wrb.model.SeqModel;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
 
-public interface SeqRepo {
-    public SeqModel getItem(String key);
+@Repository
+public class SeqRepo {
 
-    public void addItem(SeqModel item);
+    public static final String KEY = "SEQ";
+    private RedisTemplate<String, SeqModel> redisTemplate;
+    private HashOperations hashOperations;
 
-    public void deleteItem(String key);
+    public SeqRepo(RedisTemplate<String, SeqModel> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        hashOperations = redisTemplate.opsForHash();
+    }
 
-    public void updateItem(SeqModel item);
+    /*Getting a specific item by item id from table*/
+    public SeqModel getItem(String id) {
+        return (SeqModel) hashOperations.get(KEY, id);
+    }
+
+    /*Adding an item into redis database*/
+    public void addItem(SeqModel seqModel) {
+        hashOperations.put(KEY, seqModel.getId(), seqModel);
+    }
+
+    /*delete an item from database*/
+    public void deleteItem(String id) {
+        hashOperations.delete(KEY, id);
+    }
+
+    /*update an item from database*/
+    public void updateItem(SeqModel seqModel) {
+        addItem(seqModel);
+    }
 }
